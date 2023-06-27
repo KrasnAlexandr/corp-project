8ce811949dae19f6a2f47be23fb8dc8a96377a36
Krasnyansky Alexandr <krasnyansky.alexandr@gmail.com>
2023-06-27 14:42:17 +0300

    21 complete

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/classNames';
+import { useTranslation } from 'react-i18next';
+import cls from './NotFoundPage.module.scss';
+
+interface NotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+  const { t } = useTranslation('not-found');
+
+  return (
+    <div className={classNames(cls.NotFoundPage, {}, [className])}>{t('not_found_page')}</div>
+  );
+};
--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -3,5 +3,17 @@ import { classNames } from './classNames';
 describe('classNames', () => {
   test('test', () => {
     expect(classNames('someClass', {}, [])).toBe('someClass');
+    expect(classNames(
+      'main-class',
+      { mod1: true, mod2: false },
+      ['additional-class-1', 'additional-class-2'],
+    ))
+      .toBe('main-class additional-class-1 additional-class-2 mod1');
+    expect(classNames(
+      'main-class',
+      {},
+      ['additional-class-1', 'additional-class-2'],
+    ))
+      .toBe('main-class additional-class-1 additional-class-2');
   });
 });
